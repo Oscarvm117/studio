d3b640a81db2e093138668ef9d62ce7519c0f33c
Monstertrack <Oscarelmejordj@gmail.com>
2025-11-17 02:18:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8575967287-eb7a5",
+  "appId": "1:311345247264:web:5a53691de9ccc7a0357845",
+  "apiKey": "AIzaSyC0gDxUxOpp5ohVlxuXzyyq9kqJkmdEex8",
+  "authDomain": "studio-8575967287-eb7a5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "311345247264"
+};
